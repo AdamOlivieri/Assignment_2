77b747665ca55c94b2996fd7d193eecd5e897b13
AdamOlivieri <adam.olivieri@uoit.net>
2016-03-22 01:55:46 -0500

    Initial Skeleton Code for UI

---
+++ Assignment 2/src/sample/Main.java
@@ -0,0 +1,84 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.*;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.image.*;
+import javafx.collections.*;
+import javafx.event.*;
+import javafx.scene.control.TableColumn.CellEditEvent;
+import javafx.scene.control.cell.*;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class Main extends Application {
+    private Stage window;
+    private BorderPane layout;
+    private TableView</*StudentRecord*/> table;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("FileSharer v1.0");
+
+        table = new TableView<>();
+        //table.setItems();
+        table.setEditable(false);
+
+        GridPane editArea = new GridPane();
+        editArea.setPadding(new Insets(10, 10, 10, 10));
+        editArea.setVgap(10);
+        editArea.setHgap(10);
+
+        /*TableColumn<Client,String> clientColumn = null;
+        clientColumn = new TableColumn<>();
+        clientColumn.setMinWidth(300);
+
+        TableColumn<Server,String> serverColumn = null;
+        serverColumn = new TableColumn<>();
+        serverColumn.setMinWidth(300);*/
+
+        Button uploadButton = new Button("Upload");
+        uploadButton.setOnAction(new EventHandler<ActionEvent>()) {
+            @Override
+                public void handle(ActionEvent event) {
+
+                }
+        }
+
+        Button downloadButton = new Button("Download");
+        downloadButton.setOnAction(new EventHandler<ActionEvent>()) {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        }
+
+
+
+        editArea.add(downloadButton, 0, 0);
+        editArea.add(uploadButton, 1, 0);*/
+        table.getColumns().add(clientColumn);
+        table.getColumns().add(serverColumn);
+
+        layout = new BorderPane();
+        layout.setCenter(table);
+
+        Scene scene = new Scene(layout, 900, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
